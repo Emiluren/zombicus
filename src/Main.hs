7d2e1832e88faabf2bf742255833ad8f4791df99
Emil Segerbäck <emilsegerback@gmail.com>
2017-07-15 13:06:21 +0200

    Finish simpleHomoSapiens

--- src/Main.hs
+++ src/Main.hs
@@ -11,8 +11,6 @@ import SDL (($=), Point(P))
 import qualified SDL
 import qualified SDL.Image
 
-import ReflexHost (SdlApp)
-
 data RenderData = RenderData
 
 data CharacterType = Sapiens | Zombicus
@@ -20,31 +18,71 @@ data CharacterType = Sapiens | Zombicus
 data Character =
     Character { characterId :: Int
               , characterType :: CharacterType
-              , pos :: V2 Float
-              , velocity :: V2 Float
+              , pos :: V2 Double
+              , velocity :: V2 Double
               }
 
 data Trajectory =
-    Trajectory { rng :: StdGen
-               , t0 :: Float
-               , orig :: V2 Float
-               , period :: Float
-               , angle :: Float
-               , velocity :: V2 Float
+    Trajectory { t0 :: Double
+               , orig :: V2 Double
+               , period :: Double
+               , velocity :: V2 Double
                }
 
-positionAt :: Trajectory -> Float -> V2 Float
+speed :: Double
+speed = 80
+
+makeTrajectory :: StdGen -> Double -> V2 Double -> Trajectory
+makeTrajectory rng t0_ orig_ =
+    let rndPeriod:rndAngle:_ = randoms rng :: [Double]
+        angle = rndAngle * pi * 2
+    in Trajectory { t0 = t0_
+                  , orig = orig_
+                  , period = rndPeriod + 0.5
+                  , velocity = V2 (sin angle) (cos angle) ^* speed
+                  }
+
+positionAt :: Trajectory -> Double -> V2 Double
 positionAt (Trajectory {t0 = startTime, orig = origPos, velocity = vel}) t =
     vel ^* (t - startTime) + origPos
 
-simpleHomoSapiens :: (Reflex t, MonadHold t m, MonadFix m) => Int -> V2 Float -> Behavior t Float -> Event t () -> StdGen -> m (Behavior t Character)
+changeTrajectory :: (Reflex t) =>
+    Behavior t Trajectory -> Behavior t Double -> () -> PushM t (Maybe ())
+changeTrajectory traj time () = do
+    traj_ <- sample traj
+    t <- sample time
+    if t - t0 traj_ >= period traj_
+        then return $ Just ()
+        else return Nothing
+
+newTrajectory :: (Reflex t) =>
+    Behavior t Trajectory -> Behavior t Double -> StdGen ->
+    () -> PushM t (StdGen, Trajectory)
+newTrajectory traj time gen () = do
+    let (g, g') = split gen
+    t <- sample time
+    traj_ <- sample traj
+    return (g', makeTrajectory g t $ positionAt traj_ t)
+
+generators :: StdGen -> [StdGen]
+generators g = let (g', g'') = split g in g' : generators g''
+
+simpleHomoSapiens :: (Reflex t, MonadHold t m, MonadFix m) =>
+    Int -> V2 Double -> Behavior t Double -> Event t () -> StdGen -> m (Behavior t Character)
 simpleHomoSapiens self posInit time eTick rng = do
-    let speed = 80
-        rn1:rn2:_ = randoms rng :: [Float]
-    return . pure $
-        Character { characterId = self
-                  , characterType = Sapiens
-                  }
+    let gt':gt:_ = generators rng
+    rec
+        let eChange = push (changeTrajectory traj time) eTick
+        t <- sample time
+        traj <- hold (makeTrajectory gt' t posInit) =<<
+            mapAccumM_ (newTrajectory traj time) gt eChange
+
+    traj_ <- sample traj
+    return . pure $ Character { characterId = self
+                              , characterType = Sapiens
+                              , pos = positionAt traj_ t
+                              , velocity = velocity (traj_ :: Trajectory)
+                              }
 
 reflexGuest :: StdGen -> SdlApp RenderData
 reflexGuest rnd e = do
